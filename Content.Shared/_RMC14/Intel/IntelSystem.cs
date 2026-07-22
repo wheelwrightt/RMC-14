a0d1ef0a6711785fe365463e8fe53f01c36025aa
wheelwrightt <174839295+wheelwrightt@users.noreply.github.com>
2026-07-22 21:31:37 +0200

    colour

--- Content.Shared/_RMC14/Intel/IntelSystem.cs
+++ Content.Shared/_RMC14/Intel/IntelSystem.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using Content.Shared._RMC14.Areas;
 using Content.Shared._RMC14.ARES;
 using Content.Shared._RMC14.ARES.Logs;
@@ -723,7 +723,7 @@ public sealed class IntelSystem : EntitySystem
         if (!storeGlobal)
             return AddPersonalClue(user, target, clue);
 
-        _popup.PopupEntity(clue, target, user, PopupType.Medium);
+        _popup.PopupEntity(FormattedMessage.RemoveMarkupOrThrow(clue), target, user, PopupType.Medium);
 
         RemovePersonalClueFromAll(target);
 
